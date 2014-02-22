c4d90c9dd1a85644ad982af2f71be8926121f560
Dan Cameron <dan@sproutventure.com>
2014-02-21 17:10:45 -0800

    docs.

--- classes/Notifications.php
+++ classes/Notifications.php
@@ -31,6 +31,15 @@ class SEC_Reward_Notifications extends Group_Buying_Controller {
 		}
 	}
 
+	////////////////////////////////
+	// Notification Registration //
+	////////////////////////////////
+
+	/**
+	 * Register notifications
+	 * @param  array $notifications 
+	 * @return array                
+	 */
 	public function register_notification_type( $notifications ) {
 		$notifications[self::NOTIFICATION_TYPE] = array(
 			'name' => gb__( 'Reward Notification' ),
@@ -52,81 +61,11 @@ class SEC_Reward_Notifications extends Group_Buying_Controller {
 		return $notifications;
 	}
 
-	public function maybe_send_notifications() {
-		$current_month = date( self::$period_date_format, current_time('timestamp' ) );
-		if ( $current_month > self::$last_email_sent ) {
-			self::send_notifications( TRUE );
-		}
-		self::send_notifications();	
-	}
-
-
-	public static function send_notifications( $periodic_send = FALSE ) {
-		$users = self::find_users_to_get_notified( $periodic_send );
-		foreach ( $users as $user ) {
-			self::send_notification( $user );
-		}
-		self::update_last_email_sent_time();
-	}
-
-
-	public function find_users_to_get_notified( $first_timers = FALSE ) {
-		$users = array();
-		$query_args = array( 'fields' => array( 'ID', 'user_email' ), 'meta_query' => array() );
-		
-		// First timers don't have a meta for the last time they've received 
-		// an update notification.
-		if ( $first_timers ) {
-			$query_args['meta_query']['relation'] = 'OR';
-			$query_args['meta_query'][] = array(
-											'key' => self::USER_META_SENT,
-											'compare' => 'NOT EXISTS'
-											);
-		}
-
-		// Last sent before the current month. 
-		$current_month = date( self::$period_date_format, current_time('timestamp' ) );
-		$query_args['meta_query'][] = array(
-										'key' => self::USER_META_SENT,
-										'value' => $current_month,
-										'compare' => '<'
-										);
-
-		$users = get_users( $query_args );
-
-		return $users;
-	}
-
-	public function send_notification( $user ) {
-		$user_id = $user->ID;
-		$recipient = $user->user_email;
-		$balance = (int)gb_get_account_balance( $user_id, 'balance' );
-		$reward_points = (int)gb_get_account_balance( $user_id, 'points' );
-		$data = array(
-			'user_id' => $user_id,
-			'account_balance' => $balance,
-			'account_rewards' => $reward_points
-		);
-		// If the account has rewards or an account balance
-		if ( $balance > 0 || $reward_points > 0 ) {
-			Group_Buying_Notifications::send_notification( self::NOTIFICATION_TYPE, $data, $recipient );
-		}
-		else {
-			Group_Buying_Notifications::send_notification( self::NOTIFICATION_TYPE_WO, $data, $recipient );
-		}
-		do_action( 'sec_rewards_notification_sent', $user );
-	}
-
 	/**
-	 * Updated by month number
-	 * @param  int $time 
-	 * @return        
+	 * Register the shortcodes
+	 * @param  array $default_shortcodes 
+	 * @return array                     
 	 */
-	public function update_last_email_sent_time() {
-		self::$last_email_sent = date( self::$period_date_format, current_time('timestamp' ) );
-		update_option( self::EMAIL_SENT, self::$last_email_sent );
-	}
-
 	public function register_notification_shortcodes( $default_shortcodes ) {
 		$summary_report_shortcodes = array(
 				'account_balance' => array(
@@ -155,6 +94,110 @@ class SEC_Reward_Notifications extends Group_Buying_Controller {
 			$rewards = $data['account_rewards'];
 		}
 		return $rewards;
+	}
+
+	/////////////////////////////
+	// Send the notifications //
+	/////////////////////////////
+
+
+	/**
+	 * Send a notification if the term has passed; by comparing the current month number to that of the last send
+	 * @return  
+	 */
+	public function maybe_send_notifications() {
+		$current_month = date( self::$period_date_format, current_time('timestamp' ) );
+		if ( $current_month > self::$last_email_sent ) {
+			self::send_notifications();
+		}
+	}
+
+	/**
+	 * Find the users to be notified and send the notification
+	 * @param  boolean $periodic_send Is this a notification that is sent out to all users or just those that
+	 * haven't received the latest notification
+	 * @return                  
+	 */
+	public static function send_notifications( $periodic_send = TRUE ) {
+		$users = self::find_users_to_get_notified( $periodic_send );
+		foreach ( $users as $user ) {
+			self::send_notification( $user );
+		}
+		self::update_last_email_sent_time();
+	}
+
+	/**
+	 * Find users to get notified using meta querys
+	 * @param  boolean $first_timers Find users without meta of the last notification they received.
+	 * @return 
+	 */
+	public function find_users_to_get_notified( $first_timers = TRUE ) {
+		$users = array();
+		$query_args = array( 'fields' => array( 'ID', 'user_email' ), 'meta_query' => array() );
+		
+		// First timers don't have a meta for the last time they've received 
+		// an update notification.
+		if ( $first_timers ) {
+			$query_args['meta_query']['relation'] = 'OR';
+			$query_args['meta_query'][] = array(
+											'key' => self::USER_META_SENT,
+											'compare' => 'NOT EXISTS'
+											);
+		}
+
+		// Last sent before the current month. 
+		$current_month = date( self::$period_date_format, current_time('timestamp' ) );
+		$query_args['meta_query'][] = array(
+										'key' => self::USER_META_SENT,
+										'value' => $current_month,
+										'compare' => '<'
+										);
+
+		$users = get_users( $query_args );
+
+		return $users;
+	}
+
+	/**
+	 * Send the notificaiton to the user and update the user's meta.
+	 * Checks to see if the user has any type of balance and destinguishes which notification to send.
+	 * @param  object  $user      ID and user_email
+	 * @param  integer $date_sent current month
+	 * @return              
+	 */
+	public function send_notification( $user, $date_sent = 0 ) {
+		$user_id = $user->ID;
+		$recipient = $user->user_email;
+		$balance = (int)gb_get_account_balance( $user_id, 'balance' );
+		$reward_points = (int)gb_get_account_balance( $user_id, 'points' );
+		$data = array(
+			'user_id' => $user_id,
+			'account_balance' => $balance,
+			'account_rewards' => $reward_points
+		);
+		// If the account has rewards or an account balance
+		if ( $balance > 0 || $reward_points > 0 ) {
+			Group_Buying_Notifications::send_notification( self::NOTIFICATION_TYPE, $data, $recipient );
+		}
+		else {
+			Group_Buying_Notifications::send_notification( self::NOTIFICATION_TYPE_WO, $data, $recipient );
+		}
+
+		// Flag that the user has received a notification
+		$current_month = date( self::$period_date_format, current_time('timestamp' ) );
+		update_usermeta( $user_id, self::USER_META_SENT, $current_month );
+
+		do_action( 'sec_rewards_notification_sent', $user );
+	}
+
+	/**
+	 * Updated by month number
+	 * @param  int $time 
+	 * @return        
+	 */
+	public function update_last_email_sent_time() {
+		self::$last_email_sent = date( self::$period_date_format, current_time('timestamp' ) );
+		update_option( self::EMAIL_SENT, self::$last_email_sent );
 	}
 
 
